5dece8e1a6042db965202a3e88399c9caca299f4
Sebastian Flores <fles.sebas26@gmail.com>
2025-02-03 23:58:19 -0500

    feat: lsp

--- lua/sefles/plugins/rose-pine.lua
+++ lua/sefles/plugins/rose-pine.lua
@@ -3,6 +3,8 @@ return {
   priority = 1000,
   name = "rose-pine",
   config = function()
-    vim.cmd([[colorscheme rose-pine]])
+    vim.cmd.colorscheme("rose-pine")
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   end,
 }
