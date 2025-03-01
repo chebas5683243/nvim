eee394c10c8a945144df3aad29c8bb50fab866ab
Sebastian Flores <fles.sebas26@gmail.com>
2025-03-01 18:27:45 -0500

    feat: add comment and formatting

--- lua/sefles/plugins/colorscheme.lua
+++ lua/sefles/plugins/colorscheme.lua
@@ -1,8 +1,10 @@
 return {
-  "folke/tokyonight.nvim",
+  "rose-pine/neovim",
+  name = "rose-pine",
   config = function()
-    vim.cmd("colorscheme tokyonight-night")
+    vim.cmd("colorscheme rose-pine-main")
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   end
 }
+
